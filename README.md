48f6799e99dde8bbeb8480f22d8be4d4ad2dd475
AndrewGertig <andrew.gertig@gmail.com>
2011-05-20 23:39:59 -0400

    More readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 What Does it do?
 ----------------
 
-Compatible with Rails 3.0.x (Rails 3.1.x support is coming)
+Compatible with Rails 3.0.x (Rails 3.1.x support is coming).
 
 backbonejs-rails is similar to jquery-rails. It adds the Javascript files that you need to create an application that uses backbone.js as a JavaScript MVC. This gem will get the most recent version of these files from their master branches on github.
 
@@ -21,6 +21,19 @@ For templates I have included ICanHaz.js
 Upon installation the files are added to your javascript defaults so that you can still use the following in your views/layouts/application.html.erb file:
 
     <%= javascript_include_tag :defaults %>
+    
+What else?
+----------
+
+It also creates a file called "config/initializers/backbone.rb" which contains the line:
+
+    ActiveRecord::Base.include_root_in_json = false
+    
+Which is what you need to return JSON to Backbone the way it likes it. Otherwise you would need to do something like this:
+    
+    def create
+        render :json => Person.create(:name => params[:name], :age => params[:age])
+    end
 
 Installation
 ------------
@@ -40,3 +53,6 @@ Usage
     $ rails g backbonejs:install
 
 If you have already installed jquery and removed prototype then you can skip that step.
+
+You can find my [screencast on Backbone.js and Rails 3 here](http://andrewgertig.com/2011/05/rails-backbone-js-example-screencast/)
+
---
+++ lib/generators/backbonejs/install/install_generator.rb
@@ -0,0 +1,66 @@
+require 'rails'
+require 'curb'
+
+module Backbonejs
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      @@backbone_version   = "0.3.3"
+      @@underscore_version = "1.1.6"
+      @@icanhaz_version = "0.9" 
+
+      desc "This generator installs backbone.js #{@@backbone_version}, underscore.js #{@@underscore_version}, json2.js, and (optionally) icanhaz.js #{@@icanhaz_version}"
+      class_option :ich, :type => :boolean, :default => false, :desc => "Include ICanHaz.js"
+      source_root File.expand_path('../../../../../vendor/assets/javascripts', __FILE__)
+      
+      def fetch_libraries
+        url_backbone = "http://documentcloud.github.com/backbone/backbone.js"
+        url_backbone_min = "http://documentcloud.github.com/backbone/backbone-min.js"
+        url_underscore = "http://documentcloud.github.com/underscore/underscore.js"
+        url_underscore_min = "http://documentcloud.github.com/underscore/underscore-min.js"
+        url_icanhaz = "https://github.com/andyet/ICanHaz.js/raw/master/ICanHaz.js"
+        url_icanhaz_min = "https://github.com/andyet/ICanHaz.js/raw/master/ICanHaz.min.js"
+        url_json2 = "https://github.com/douglascrockford/JSON-js/raw/master/json2.js"
+
+        urls = {"backbone" => url_backbone, "backbone.min" => url_backbone_min, "underscore" => url_underscore, "underscore.min" => url_underscore_min,
+               "json2" => url_json2, "icanhaz" => url_icanhaz, "icanhaz.min" => url_icanhaz_min }
+
+        urls.each do |url|
+          file_url = url[1]
+          file_name = url[0]     
+          new_file = "public/javascripts/#{file_name}.js"
+
+          
+          begin         
+            c = Curl::Easy.new(file_url)
+            c.perform
+          
+            if File.exist?(new_file)  
+              puts "Skipping #{file_name}.js because it already exists"  
+            else  
+              puts "Generating #{new_file}"  
+              create_file new_file
+              append_to_file new_file, c.body_str
+            end
+          
+          rescue
+            puts "Connection to #{the_url} failed!"
+            puts "Falling back to copying over a, most likely, older version."
+            
+            say_status("copying", "#{file_name}.js", :green)
+            copy_file "#{file_name}.js", "public/javascripts/#{file_name}.js"
+          end
+
+        end
+      end
+
+      def copy_json_false
+        backbone_rb =  "config/initializers/backbone.rb"
+        create_file backbone_rb
+        say_status("creating", "backbone.rb - excludes root in json return just like Backbone likes it.", :green)
+        append_to_file backbone_rb, "ActiveRecord::Base.include_root_in_json = false"
+        #copy_file "backbone.js", "public/javascripts/jquery.js"
+      end
+
+    end
+  end
+end
