c8551f43ddc8d8ca442658fdc3ea1f757e45e0cc
TweeKane <tweekane@gmail.com>
2011-06-08 17:02:56 +0400

    Change " to '

--- lib/generators/backbonejs/install/install_generator.rb
+++ lib/generators/backbonejs/install/install_generator.rb
@@ -5,27 +5,27 @@ require 'uri'
 module Backbonejs
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
-      @@backbone_version   = "0.3.3"
-      @@underscore_version = "1.1.6"
-      @@icanhaz_version = "0.9" 
+      @@backbone_version   = '0.3.3'
+      @@underscore_version = '1.1.6'
+      @@icanhaz_version = '0.9'
 
       desc "This generator installs backbone.js #{@@backbone_version}, underscore.js #{@@underscore_version}, json2.js, and (optionally) icanhaz.js #{@@icanhaz_version}"
-      class_option :ich, :type => :boolean, :default => false, :desc => "Include ICanHaz.js"
+      class_option :ich, :type => :boolean, :default => false, :desc => 'Include ICanHaz.js'
       source_root File.expand_path('../../../../../vendor/assets/javascripts', __FILE__)
 
       def fetch_libraries
         urls = {
-          "backbone" => "http://documentcloud.github.com/backbone/backbone.js",
-          "backbone.min" => "http://documentcloud.github.com/backbone/backbone-min.js",
-          "underscore" => "http://documentcloud.github.com/underscore/underscore.js",
-          "underscore.min" => "http://documentcloud.github.com/underscore/underscore-min.js",
-          "json2" => "https://raw.github.com/douglascrockford/JSON-js/master/json2.js"
+          'backbone' => 'http://documentcloud.github.com/backbone/backbone.js',
+          'backbone.min' => 'http://documentcloud.github.com/backbone/backbone-min.js',
+          'underscore' => 'http://documentcloud.github.com/underscore/underscore.js',
+          'underscore.min' => 'http://documentcloud.github.com/underscore/underscore-min.js',
+          'json2' => 'https://raw.github.com/douglascrockford/JSON-js/master/json2.js'
         }
 
         if options[:ich]
           urls.merge!(
-            "icanhaz" => "https://raw.github.com/andyet/ICanHaz.js/master/ICanHaz.js",
-            "icanhaz.min" => "https://raw.github.com/andyet/ICanHaz.js/master/ICanHaz.min.js"
+            'icanhaz' => 'https://raw.github.com/andyet/ICanHaz.js/master/ICanHaz.js',
+            'icanhaz.min' => 'https://raw.github.com/andyet/ICanHaz.js/master/ICanHaz.min.js'
           )
         end
 
@@ -60,13 +60,14 @@ module Backbonejs
       end
 
       def copy_json_false
-        backbone_rb =  "config/initializers/backbone.rb"
+        backbone_rb =  'config/initializers/backbone.rb'
         create_file backbone_rb
-        say_status("creating", "backbone.rb - excludes root in json return just like Backbone likes it.", :green)
-        append_to_file backbone_rb, "ActiveRecord::Base.include_root_in_json = false"
+        say_status('creating', 'backbone.rb - excludes root in json return just like Backbone likes it.', :green)
+        append_to_file backbone_rb, 'ActiveRecord::Base.include_root_in_json = false'
         #copy_file "backbone.js", "public/javascripts/jquery.js"
       end
 
     end
   end
 end
+
