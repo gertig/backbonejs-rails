038f78c8289588a995ef074605b88880eb3f4c6b
AndrewGertig <andrew.gertig@gmail.com>
2012-02-24 20:38:06 -0500

    updating to curent versions of backbone, icanhaz and underscore

--- lib/backbonejs-rails/version.rb
+++ lib/backbonejs-rails/version.rb
@@ -1,5 +1,5 @@
 module Backbonejs
   module Rails
-    VERSION = "0.0.5"
+    VERSION = "0.0.6"
   end
 end
--- lib/generators/backbonejs/install/install_generator.rb
+++ lib/generators/backbonejs/install/install_generator.rb
@@ -75,9 +75,9 @@ else
   module Backbonejs
     module Generators
       class InstallGenerator < ::Rails::Generators::Base
-        @@backbone_version   = '0.5.3'
-        @@underscore_version = '1.1.7'
-        @@icanhaz_version = '0.9'
+        @@backbone_version   = '0.9.1'
+        @@underscore_version = '1.3.1'
+        @@icanhaz_version = '0.10'
         
         desc "This generator installs backbone.js #{@@backbone_version}, underscore.js #{@@underscore_version}, json2.js, and (optionally) icanhaz.js #{@@icanhaz_version}"
         class_option :ich, :type => :boolean, :aliases => "-i", :default => false, :desc => 'Include ICanHaz.js'
