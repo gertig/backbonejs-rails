7370582d38e89687eacc610e721f1a2e2551cc53
AndrewGertig <andrew.gertig@gmail.com>
2013-07-07 20:57:07 -0400

    moving away from Rails 3.0

--- lib/backbonejs-rails.rb
+++ lib/backbonejs-rails.rb
@@ -2,11 +2,7 @@ require 'rails/generators/base'
 
 module Backbonejs
   module Rails #Generators
-    if ::Rails.version < "3.1"
-      require 'backbonejs-rails/railtie'
-    else
-      require 'backbonejs-rails/engine'
-    end
+    require 'backbonejs-rails/engine'
     require 'backbonejs-rails/version'
   end
 end
